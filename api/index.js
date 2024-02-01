274c069762b5f26feb403675d4940861edc04ad1
Dhiraj-tech <dhirajrayofficial@gmail.com>
2024-02-01 18:25:09 +0545

    Final commit

--- api/index.js
+++ api/index.js
@@ -10,12 +10,6 @@ config()
 const app = express()
 
 app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  })
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
